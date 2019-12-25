3cf902d0d1ab403371ba24ddc42b8b19ac2bc132
boybingo9x <58473396+boybingo9x@users.noreply.github.com>
2019-12-25 21:12:57 +0700

    Initialize project
    1st baseline:
    Login windows

---
+++ My1stProject/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace My1stProject
+{
+    partial class fForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.ckRemember = new System.Windows.Forms.CheckBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(237, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phantom Game";
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUserName.Location = new System.Drawing.Point(132, 88);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(75, 17);
+            this.lbUserName.TabIndex = 1;
+            this.lbUserName.Text = "UserName";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(132, 121);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(69, 17);
+            this.lbPassword.TabIndex = 2;
+            this.lbPassword.Text = "Password";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(232, 85);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(278, 20);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(232, 118);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(278, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // ckRemember
+            // 
+            this.ckRemember.AutoSize = true;
+            this.ckRemember.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckRemember.Location = new System.Drawing.Point(232, 153);
+            this.ckRemember.Name = "ckRemember";
+            this.ckRemember.Size = new System.Drawing.Size(119, 21);
+            this.ckRemember.TabIndex = 5;
+            this.ckRemember.Text = "Remember me";
+            this.ckRemember.UseVisualStyleBackColor = true;
+            // 
+            // btLogin
+            // 
+            this.btLogin.BackColor = System.Drawing.Color.Lime;
+            this.btLogin.ForeColor = System.Drawing.Color.Indigo;
+            this.btLogin.Location = new System.Drawing.Point(232, 190);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(119, 24);
+            this.btLogin.TabIndex = 6;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = false;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.BackColor = System.Drawing.Color.Red;
+            this.btExit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btExit.Location = new System.Drawing.Point(398, 190);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(112, 24);
+            this.btExit.TabIndex = 7;
+            this.btExit.Text = "Exit";
+            this.btExit.UseVisualStyleBackColor = false;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // fForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(710, 307);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.ckRemember);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUserName);
+            this.Controls.Add(this.label1);
+            this.Name = "fForm1";
+            this.Text = "TestForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUserName;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox ckRemember;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Button btExit;
+    }
+}
+
---
+++ My1stProject/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace My1stProject
+{
+    public partial class fForm1 : Form
+    {
+        public fForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\German\Learning\C#\My1stProject\My1stProject\DataTest.mdf;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count (*) From Login where Username = '" + txtUserName.Text + "' and Password = '" + txtPassword.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Main ss = new Main();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please try again !!!");
+            }
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
